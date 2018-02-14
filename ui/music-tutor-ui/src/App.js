7c8c38fe1928ea64fa80ded71c9b283c55a75640
Matt Hooper <mhooper@scottlogic.com>
2018-02-14 17:36:43 +0000

    Created basic PupilManager component

--- ui/music-tutor-ui/src/App.js
+++ ui/music-tutor-ui/src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PupilManager from './containers/PupilManager'
 
-class App extends Component {
-  render() {
+const App = ()=> {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <h1>Music Tutor</h1>
+          <PupilManager />
       </div>
     );
-  }
 }
 
 export default App;
---
+++ ui/music-tutor-ui/src/containers/PupilManager.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+
+const pupilList = [
+    {
+        "Pupil ID": "P1",
+        "Name": "Matt",
+        "Tutor ID": "T1",
+        "Tutor Name": "The Piano Man",
+        "Contact Name" :"Matt",
+        "Contact phone" : "07950 212121",
+        "Contact email" : "my.email@gmail.com",
+        "Start Date": "2018-01-01",
+        "Instrument": "Piano",
+        "Lesson Rate": "15.0",
+        "Account Balance": "0.00",
+        "Lesson Interval": "7",
+        "Next Lesson Date": "2018-02-14",
+        "Profile Picture": "https://musictutorapiba1f.blob.core.windows.net/profilepics/M75.jpg",
+        "Notes": ""
+    },
+    {
+        "Pupil ID": "P2",
+        "Name": "Mark",
+        "Tutor ID": "T1",
+        "Tutor Name": "The Piano Man",
+        "Contact Name" :"Mark",
+        "Contact phone" : "07950 212121",
+        "Contact email" : "p2@email.com",
+        "Start Date": "2018-01-01",
+        "Instrument": "Piano",
+        "Lesson Rate": "15.0",
+        "Account Balance": "0.00",
+        "Lesson Interval": "7",
+        "Next Lesson Date": "2018-02-14",
+        "Profile Picture": "https://musictutorapiba1f.blob.core.windows.net/profilepics/85.jpg",
+        "Notes": ""
+    },
+    {
+        "Pupil ID": "P3",
+        "Name": "Mary",
+        "Tutor ID": "T1",
+        "Tutor Name": "The Piano Man",
+        "Contact Name" :"Mary",
+        "Contact phone" : "07950 212121",
+        "Contact email" : "p3@email.com",
+        "Start Date": "2018-01-01",
+        "Instrument": "Piano",
+        "Lesson Rate": "15.0",
+        "Account Balance": "0.00",
+        "Lesson Interval": "7",
+        "Next Lesson Date": "2018-02-14",
+        "Profile Picture": "https://musictutorapiba1f.blob.core.windows.net/profilepics/W85.jpg",
+        "Notes": ""
+    },
+    {
+        "Pupil ID": "P4",
+        "Name": "Martha",
+        "Tutor ID": "T1",
+        "Tutor Name": "The Piano Man",
+        "Contact Name" :"Martha",
+        "Contact phone" : "07950 212121",
+        "Contact email" : "p4@email.com",
+        "Start Date": "2018-01-01",
+        "Instrument": "Piano",
+        "Lesson Rate": "15.0",
+        "Account Balance": "0.00",
+        "Lesson Interval": "7",
+        "Next Lesson Date": "2018-02-14",
+        "Profile Picture": "https://musictutorapiba1f.blob.core.windows.net/profilepics/21.jpg",
+        "Notes": ""
+    }
+];
+
+class PupilManager extends Component {
+    state = {
+        pupils: pupilList
+    }
+    render() {
+      return(
+        <div>Pupils: { this.state.pupils.length}</div>
+      );
+    }
+}
+
+export default PupilManager;
